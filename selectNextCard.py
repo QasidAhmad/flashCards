7223b9a57d34c8c59bfe8852b481f0cb3d15c144
QasidAhmad <syed.qasid.ahmad@gmail.com>
2020-08-19 12:49:53 +0100

    selecting next card by random

--- selectNextCard.py
+++ selectNextCard.py
@@ -1,8 +1,13 @@
+import random
 
+def selectNextCard(questionsHistory):
 
-def selectNextCard(questionsHistory)
-{
 	# select next card based on history of reponses
+    
+    # simulating card selection with random selection
+    nextCardID = random.randrange(200)
+	
+    return nextCardID
 
-	return nextCardID
-}
+#test =1 
+#print(selectNextCard(test))
