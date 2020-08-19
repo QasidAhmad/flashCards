4a3825a8d04097b05dccbaf59e1af8582b0889ac
woody <phy5m2w@googlemail.com>
2020-08-19 10:22:59 +0100

    Restructured Main function
    Signed-off-by: woody <phy5m2w@googlemail.com>

--- displayquestion.py
+++ displayquestion.py
@@ -52,7 +52,57 @@ def askDifficulty():
     if (response<0): response=0
     if (response>9): response=9
     return response
-    
+  
+import json
+import glob
 
-
+def initialiseSession():
+    print("Welcome Back! \n Please choose the deck you want to learn today:")
     
+    
+    decksAvailable=glob.glob('./Decks/*.json')
+    
+    for idx, file in enumerate(decksAvailable):
+        print(idx+1, end = '')
+        print(": ", end = '')
+        print(file[8:-5])  #numberical factors to remove "./Decks\" beginning and ".json" end
+    response="0"    
+    while (not(response.isdigit()) or (int(response))>len(decksAvailable) or (int(response))<1):
+        response=input("Please type a number between 1 and %d: " % len(decksAvailable))
+    with open(decksAvailable[int(response)-1], 'r') as fp:
+        questions = json.load(fp)
+    
+    print("\nYou last accessed this deck X days/weeks ago. There are %d cards in this deck, X are new to you, Y are overdue. (other stats about the deck here). How many do you want to look at in this session?" % (len(questions)-1))
+    totalCards="-1"  
+    while (not(totalCards.isdigit()) or (int(totalCards))>(len(questions)-1) or (int(totalCards))<0):
+        totalCards=input("Please type a number between 1 and %d: " % (len(questions)-1))
+        
+    totalCards=int(totalCards)
+    return totalCards, questions
+    
+def giveFeedback(checkA, answer):
+    if checkA[0]:
+        print("Correct!, it took you %.1f seconds" % checkA[1])
+    else:
+        print("\nWrong!")
+        print("The Correct Answer was ", end = '')
+        print(answer)
+        
+def showProgress(Quest, totalCards):
+        
+    print("\ncompleted %d questions out of %d" % ((Quest+1), totalCards))
+    for j in range(40):
+        if (j > ((Quest+1) * 40 /totalCards)):
+            print("_", end = '')  #print a progress bar
+        else:
+            print("█", end = '')
+            
+def endSession(totalCards):
+    print("\n you got X correct out of %d cards" % totalCards) 
+    
+def enterToContinue():  #gives user chance to have a break or exit
+    print('\nPress "Enter" to continue')
+    checkexit=input("")  #gives a chance to exit early
+    if checkexit=="x":
+        print('Goodbye...')
+        return
