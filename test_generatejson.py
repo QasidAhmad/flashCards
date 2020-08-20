1d6897d0773329e86d7710a803c756b30c1754e3
QasidAhmad <syed.qasid.ahmad@gmail.com>
2020-08-20 12:37:42 +0100

    first attempt to test createJSON (failed)

--- test_generatejson.py
+++ test_generatejson.py
@@ -4,6 +4,26 @@ Created on Thu Aug 20 11:48:52 2020
 
 @author: syedq
 """
+import json
+#import generatejson
+#from unittest.mock import Mock 
+#
+#def test_createJSON(self):
+#    #testFile = "TestData/jsonCreationTest.json"
+#    testDict = {"testKey": "testVal"}
+#    file = Mock()
+#    createJSON(file,testDict)
 
-import generatejson
+def createJSON(file,dict):
+    with open(file, 'w') as fp:
+        json.dump(dict, fp)
+
+def test_createJSON():
+    testFile = "TestData/jsonCreationTest.json"
+    testDict = {"testKey":"testVal"}
+    createJSON(testFile,testDict)
+    
+    dictFromJson = json.loads(testFile)
+    
+    assert dictFromJson["testKey"] == "testVal"
 
